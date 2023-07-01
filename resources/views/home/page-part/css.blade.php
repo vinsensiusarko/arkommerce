33599cc0adc0e8e0018b53fa286d3c1e984d9a8f
vinsensiusarko <vinsensiusarka@gmail.com>
2023-07-01 18:47:54 +0700

    fix link bootstrap and jquery

--- resources/views/home/page-part/css.blade.php
+++ resources/views/home/page-part/css.blade.php
@@ -13,12 +13,12 @@
 <link href="/home/img/favicon.ico" rel="icon">
 
 <!-- Google Web Fonts -->
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+<link rel="preconnect" href="http://fonts.gstatic.com">
+<link href="http://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet">
 
 <!-- Font Awesome -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+<link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
 <!-- Libraries Stylesheet -->
 <link href="/home/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
@@ -30,6 +30,6 @@
 @vite(['resources/js/app.js'])
 
 <!-- Sweet Alert CDN -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"
+<script src="http://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"
     integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>

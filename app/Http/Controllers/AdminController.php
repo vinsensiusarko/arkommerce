5bdb9262626335e9e7e391fcc0944f63b5ca018c
vinsensiusarko <vinsensiusarka@gmail.com>
2023-07-02 05:23:58 +0700

    fix store file

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -77,8 +77,7 @@ class AdminController extends Controller
 
         $image = $request->image;
         $imagename = time().'.'.$image->getClientOriginalExtension();
-        // $request->image->move('product', $imagename);
-        $request->image->move(public_path('product'), $imagename);
+        $request->image->move(public_path().'/product/', $imagename);
 
         $product->image = $imagename;
 

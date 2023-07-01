cf67865c1c14c0e1516f9c3d2bf32c1fa26f1ef4
vinsensiusarko <vinsensiusarka@gmail.com>
2023-07-01 18:52:37 +0700

    fix bootstrap and jquery

--- resources/views/home/page-part/script.blade.php
+++ resources/views/home/page-part/script.blade.php
@@ -1,6 +1,6 @@
     <!-- JavaScript Libraries -->
-    <script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="http://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
     <script src="home/lib/easing/easing.min.js"></script>
     <script src="home/lib/owlcarousel/owl.carousel.min.js"></script>
 

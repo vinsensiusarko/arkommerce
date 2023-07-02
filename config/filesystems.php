9a9c796dbb0b30741ae944a2a70ca08fb851730f
vinsensiusarko <vinsensiusarka@gmail.com>
2023-07-03 01:12:42 +0700

    base_path

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        base_path('/public/storage') => storage_path('app/public'),
     ],
 
 ];

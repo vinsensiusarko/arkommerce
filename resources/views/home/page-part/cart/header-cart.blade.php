ce8f4fa05e61d91748881217ca416c880666bced
vinsensiusarko <vinsensiusarka@gmail.com>
2023-07-03 01:20:35 +0700

    fix header view

--- resources/views/home/page-part/cart/header-cart.blade.php
+++ resources/views/home/page-part/cart/header-cart.blade.php
@@ -36,7 +36,7 @@
                 <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
                     <a href="" class="text-decoration-none d-block d-lg-none">
                         <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                                class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                                class="text-primary font-weight-bold border px-3 mr-1">A</span>rkommerce</h1>
                     </a>
                     <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span class="navbar-toggler-icon"></span>
